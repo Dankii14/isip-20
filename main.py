deb5885218fac6d393a087266ff403156f49f93a
dankii14 <danilkasedov150404@gmail.com>
2023-02-17 11:40:26 +0300

    update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-TOKEN = '**'
+TOKEN = '***'
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
 
@@ -36,89 +36,134 @@ def func(message):
     if (message.text == "Понедельник"):
         responsepon = requests.get(pon)
         ponbs = BeautifulSoup(responsepon.text, "lxml")
-        datepon = ponbs.find(text='Понедельник').parent
+        datepon = ponbs.find(string='Понедельник').parent
         date2pon = datepon.get_text()
-        isip20pon = ponbs.find('td', text='ИСиП-20').parent
+        isip20pon = ponbs.find('td', string='ИСиП-20').parent
+        td_count=-1
+        for td in isip20pon.find_all('td'):
+            td_count += 1
+            if td_count % 2 == 1:
+                td.insert_after('\n')
         temppon = isip20pon.text
+        #team = temppon.split('\n')
+        #dd3pon = [a.replace('ИСиП-20', '').replace('\xa0', 'окно') for a in team]
+        #for i in range(len(dd3pon) - 1):
+            #if dd3pon[i] == dd3pon[i + 1]:
+                #del dd3pon[i + 1]
+            #elif dd3pon[i] == '':
+                #dd3pon.insert(i + 1, "\n")
+
+        #print(dd3pon)
+        #dd3pon=list(filter(lambda x: x != '', dd3pon))
+        #print(dd3pon)
+        #result = " ".join(dd3pon).strip()
+        #print(result)
         daspon = [temppon]
-        dd2pon = [a.replace('ИСиП-20', '').replace('\xa0\n\xa0', 'окно').replace('\n', ' ').replace('\r', '') for a in daspon]
+        dd2pon = [a.replace('ИСиП-20', '').replace('\xa0\n\xa0', 'окно') for a in daspon]
         hipon = ' '.join(dd2pon)
         date3pon = date2pon.replace('Понедельник', '').strip()
         hi2pon = hipon.strip().replace('  ', '')
-        gspon = date3pon + '\n' + hi2pon
+        gspon = date3pon + '\n' + '\n' + hi2pon
+        #gspon2 = date3pon + '\n' + result
         bot.send_message(message.chat.id, text=(gspon))
     elif (message.text == "Вторник"):
         responsevtor = requests.get(vtor)
         vtorbs = BeautifulSoup(responsevtor.text, "lxml")
-        datevtor = vtorbs.find(text='Вторник').parent
+        datevtor = vtorbs.find(string='Вторник').parent
         date2vtor = datevtor.get_text()
-        isip20vtor = vtorbs.find('td', text='ИСиП-20').parent
+        isip20vtor = vtorbs.find('td', string='ИСиП-20').parent
+        td_count = -1
+        for td in isip20vtor.find_all('td'):
+            td_count += 1
+            if td_count % 2 == 1:
+                td.insert_after('\n')
         tempvtor = isip20vtor.text
         dasvtor = [tempvtor]
-        dd2vtor = [a.replace('ИСиП-20', '').replace('\xa0\n\xa0', 'окно').replace('\n', ' ').replace('\r', '') for a in dasvtor]
+        dd2vtor = [a.replace('ИСиП-20', '').replace('\xa0\n\xa0', 'окно').replace('\r', '') for a in dasvtor]
         hivtor = ' '.join(dd2vtor)
         date3vtor = date2vtor.replace('Вторник', '').strip()
         hi2vtor = hivtor.strip().replace('  ', '')
-        gsvtor = date3vtor + '\n' + hi2vtor
+        gsvtor = date3vtor + '\n' '\n' + hi2vtor
         bot.send_message(message.chat.id, text=(gsvtor))
     elif (message.text == "Среда"):
         responsecreda = requests.get(creda)
         credabs = BeautifulSoup(responsecreda.text, "lxml")
-        datecreda = credabs.find(text='Среда').parent
+        datecreda = credabs.find(string='Среда').parent
         date2creda = datecreda.get_text()
-        isip20creda = credabs.find('td', text='ИСиП-20').parent
+        isip20creda = credabs.find('td', string='ИСиП-20').parent
+        td_count = -1
+        for td in isip20creda.find_all('td'):
+            td_count += 1
+            if td_count % 2 == 1:
+                td.insert_after('\n')
+
         tempcreda = isip20creda.text
         dascreda = [tempcreda]
-        dd2creda = [a.replace('ИСиП-20', '').replace('\xa0\n\xa0', 'окно').replace('\n', ' ').replace('\r', '') for a in
+        dd2creda = [a.replace('ИСиП-20', '').replace('\xa0\n\xa0', 'окно').replace('\r', '') for a in
                    dascreda]
         hicreda = ' '.join(dd2creda)
         date3creda = date2creda.replace('Среда', '').strip()
         hi2creda = hicreda.strip().replace('  ', '')
-        gscreda = date3creda + '\n' + hi2creda
+        gscreda = date3creda + '\n' + '\n' + hi2creda
         bot.send_message(message.chat.id, text=(gscreda))
     elif (message.text == "Четверг"):
         responsechet = requests.get(chetwerk)
         chetbs = BeautifulSoup(responsechet.text, "lxml")
-        datechet = chetbs.find(text='Четверг').parent
+        datechet = chetbs.find(string='Четверг').parent
         date2chet = datechet.get_text()
-        isip20chet = chetbs.find('td', text='ИСиП-20').parent
+        isip20chet = chetbs.find('td', string='ИСиП-20').parent
+        td_count = -1
+        for td in isip20chet.find_all('td'):
+            td_count += 1
+            if td_count % 2 == 1:
+                td.insert_after('\n')
         tempchet = isip20chet.text
         daschet = [tempchet]
-        dd2chet = [a.replace('ИСиП-20', '').replace('\xa0\n\xa0', 'окно').replace('\n', ' ').replace('\r', '') for a in
+        dd2chet = [a.replace('ИСиП-20', '').replace('\xa0\n\xa0', 'окно').replace('\r', '') for a in
                     daschet]
         hichet = ' '.join(dd2chet)
         date3chet = date2chet.replace('Четверг', '').strip()
         hi2chet = hichet.strip().replace('  ', '')
-        gschet = date3chet + '\n' + hi2chet
+        gschet = date3chet + '\n' +  "\n" + hi2chet
         bot.send_message(message.chat.id, text=(gschet))
     elif (message.text == "Пятница"):
         responsepyat = requests.get(pyat)
         pyatbs = BeautifulSoup(responsepyat.text, "lxml")
-        datepyat = pyatbs.find(text='Пятница').parent
+        datepyat = pyatbs.find(string='Пятница').parent
         date2pyat = datepyat.get_text()
-        isip20pyat = pyatbs.find('td', text='ИСиП-20').parent
+        isip20pyat = pyatbs.find('td', string='ИСиП-20').parent
+        td_count = -1
+        for td in isip20pyat.find_all('td'):
+            td_count += 1
+            if td_count % 2 == 1:
+                td.insert_after('\n')
         temppyat = isip20pyat.text
         daspyat = [temppyat]
-        dd2pyat = [a.replace('ИСиП-20', '').replace('\xa0\n\xa0', 'окно').replace('\n', ' ').replace('\r', '') for a in
+        dd2pyat = [a.replace('ИСиП-20', '').replace('\xa0\n\xa0', 'окно').replace('\r', '') for a in
                    daspyat]
         hipyat = ' '.join(dd2pyat)
         date3pyat = date2pyat.replace('Пятница', '').strip()
         hi2pyat = hipyat.strip().replace('  ', '')
-        gspyat = date3pyat + '\n' + hi2pyat
+        gspyat = date3pyat + '\n'+ '\n' + hi2pyat
         bot.send_message(message.chat.id, text=(gspyat))
     elif (message.text == "Суббота"):
         responsesub = requests.get(subbota)
         subbs = BeautifulSoup(responsesub.text, "lxml")
-        datesub = subbs.find(text='Суббота дистанционно').parent
+        datesub = subbs.find(string='Суббота дистанционно').parent
         date2sub = datesub.get_text()
-        isip20sub = subbs.find('td', text='ИСиП-20').parent
+        isip20sub = subbs.find('td', string='ИСиП-20').parent
+        td_count = -1
+        for td in isip20sub.find_all('td'):
+            td_count += 1
+            if td_count % 2 == 1:
+                td.insert_after('\n')
         tempsub = isip20sub.text
         dassub = [tempsub]
-        dd2sub = [a.replace('ИСиП-20', '').replace('\xa0\n\xa0', 'окно').replace('\n', ' ').replace('\r', '') for a in
+        dd2sub = [a.replace('ИСиП-20', '').replace('\xa0\n\xa0', 'окно').replace('\r', '') for a in
                    dassub]
         hisub = ' '.join(dd2sub)
         date3sub = date2sub.replace('Суббота дистанционно', '').strip()
         hi2sub = hisub.strip().replace('  ', '')
-        gssub = date3sub + '\n' + hi2sub
+        gssub = date3sub + '\n' + '\n' + hi2sub
         bot.send_message(message.chat.id, text=(gssub))
 bot.polling(none_stop=True)
